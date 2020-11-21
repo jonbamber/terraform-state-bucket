5bc1d59f1dab19fd0deea1b81331a6a757ca2413
jonbamber <jonbamber@hotmail.com>
2020-11-21 19:31:25 +0000

    Add terraform state file bucket
    Add configuration for:
     * an S3 bucket for terraform state files
     * a KMS key for S3 server-side encryption
     * an S3 bucket IAM policy ensuring HTTPS is always used
     * S3 Block Public Access

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # terraform-state-bucket
+
+This code sets up the AWS infrastructure needed to configure other infrastructure in an AWS account.
+
+This code configures:
+ * an S3 bucket to use for Terraform state files
+ * a KMS key for S3 server-side encryption
+ * an S3 bucket policy that ensures all access is via HTTPS
+ * S3 Block Public Access
+
+As this bucket doesn't pre-exist to store its own state file,
+the Terraform backend configuration makes use of Terraform Cloud.
---
+++ main.tf
@@ -0,0 +1,64 @@
+provider "aws" {
+  version = "~> 2.43"
+  region  = "eu-west-1"
+}
+
+terraform {
+  backend "remote" {
+    organization = "jonbamber"
+
+    workspaces {
+      name = "terraform-state-bucket"
+    }
+  }
+}
+
+data aws_iam_policy_document "state_bucket" {
+  statement {
+    sid       = "EnsureHTTPS"
+    effect    = "Deny"
+    resources = ["${aws_s3_bucket.state_bucket.arn}/*"]
+
+    actions   = ["*"]
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_kms_key" "state_bucket" {
+  description = "KMS key for encryption on the Terraform state file bucket"
+}
+
+resource "aws_s3_bucket" "state_bucket" {
+  bucket = "jonbamber-tf-state"
+  acl    = "private"
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.state_bucket.arn
+        sse_algorithm     = "aws:kms"
+      }
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "state_bucket" {
+  bucket = aws_s3_bucket.state_bucket.id
+  policy = data.aws_iam_policy_document.state_bucket.json
+}
+
+resource "aws_s3_bucket_public_access_block" "state_bucket" {
+  bucket              = aws_s3_bucket.state_bucket.id
+  block_public_acls   = true
+  block_public_policy = true
+}
