c0c15f8beee9de0b8a35a7070fcf68fed450f00a
Jon Bamber <jonbamber@hotmail.com>
2021-01-31 10:28:19 +0000

    Fully lock down public access

--- main.tf
+++ main.tf
@@ -53,7 +53,9 @@ resource "aws_s3_bucket_policy" "state_bucket" {
 }
 
 resource "aws_s3_bucket_public_access_block" "state_bucket" {
-  bucket              = aws_s3_bucket.state_bucket.id
-  block_public_acls   = true
-  block_public_policy = true
+  bucket                  = aws_s3_bucket.state_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
